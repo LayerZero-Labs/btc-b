ade7f79beae9196e9dde921bb89351012cf8c985
lz.sirΔrthurmoney() <kyle@layerzerolabs.org>
2022-11-07 18:05:22 -0800

    update token and bring in erc20

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,11 +1,18 @@
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import "@layerzerolabs/solidity-examples/contracts/mocks/MockToken.sol";
-
-contract Token is MockToken {
-    constructor(string memory _name, string memory _symbol) MockToken(_name, _symbol) {}
+contract Token is ERC20 {
+    // this is a MOCK
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+        _mint(msg.sender, 1_000_000_000 * 10**decimals()); // mint 1B to deployoooor
+    }
 
     function decimals() public pure override returns (uint8){
         return 8;
+    }
+
+    // this is a MOCK
+    function mint(address _to, uint _amount) public {
+        _mint(_to, _amount);
     }
 }
